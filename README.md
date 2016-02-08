3e9444b8a07c5c0b6cd9280164809cfbe08dde0b
Siddharth Vasu <siddharth_vasu@hotmail.co.uk>
2016-02-08 17:00:29 +0000

    fixed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # random-stuff
 Helper Functions and other useful tools that I have made.
 Very Useful!
+
+''num2latex.m'' - MATLAB script that takes in an array and returns the LaTeX representation of it as a string. Requires \usepackage{amsmath} to work.
--- num2latex.m
+++ num2latex.m
@@ -1,7 +1,7 @@
 function [str] = num2latex(x)
 %mat2latex prints out and stores the format string associated with a lateX
 %matrix from a MATLAB array.
-str = '\n\\begin{bmatrix}\n';
+str = '\\[\n\\begin{bmatrix}\n';
 cell = num2cell(x,2);
 for i=1:size(cell,1)
     a = strtrim(num2str(cell{i}));
@@ -9,6 +9,6 @@ for i=1:size(cell,1)
     a = strcat(a,'\\\\\n ');
     str = strcat(str,a);
 end
-str = strcat(str,'\\end{bmatrix}\n');
+str = strcat(str,'\\end{bmatrix}\n\\]');
 fprintf(str);
 end
